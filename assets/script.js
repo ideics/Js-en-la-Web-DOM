a89c491f22e5646b332dbdbff493cd030c65d4c4
Angel Jiménez <computerserviceec@gmail.com>
2022-11-24 21:24:29 -0500

    Mejorando codigo

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,6 @@
-(() => {
+  import checkComplete from "../components/checkComplete.js";
+  import deleteIcon from "../components/deleteIcon.js";
+
   const btn = document.querySelector('[data-form-btn]');
 
   const createTask = (evento) => {
@@ -25,31 +27,3 @@
 
   //Arrow functions o funciones anonimas
   btn.addEventListener('click', createTask);
-
-  const checkComplete = () => {
-    const i = document.createElement('i');
-    i.classList.add('far', 'fa-check-square', 'icon');
-    i.addEventListener('click', completeTask);
-    return i;
-  };
-  // Immediately invoked function expression IIFE
-  const completeTask = (event) => {
-    const element = event.target;
-    element.classList.toggle('fas');
-    element.classList.toggle('completeIcon');
-    element.classList.toggle('far');
-  };
-
-  const deleteIcon = () => {
-    const i = document.createElement("i");
-    i.classList.add('fas', 'fa-trash-alt', 'trashIcon', 'icon' );
-    i.addEventListener('click', deleteTask);
-    return i
-  }
-
-  const deleteTask = (event) => {
-    const parent = event.target.parentElement;
-    parent.remove();
-  }
-
-})();
